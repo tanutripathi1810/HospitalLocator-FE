97a6d6c39208a0c84c7b4b61b76642d6d7675a11
tanutripathi1810 <139795364+tanutripathi1810@users.noreply.github.com>
2024-08-21 23:53:16 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 //TODO 1 enter your key from geocode here
-export const GEOCODE_API_KEY="";
+export const GEOCODE_API_KEY="66c620c193f89983216947rkp2d459d";
 
 //TODO 2 enter your function app url here
-export const FUNCTION_APP_URL="http://localhost:7071/api/getHospitalsList";
+export const FUNCTION_APP_URL="https://fa-cloud-user27.azurewebsites.net/api/getHospitalsList?code=+";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
